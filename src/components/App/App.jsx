cf93c20c726d670bfeaea3956359f4ad1597a0cb
Jonathan Brandt <jwilliambrandtwork@gmail.com>
2025-04-01 17:25:34 -0500

    Project-10 Modal WIP

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -14,7 +14,44 @@ function App() {
         <Header />
         <Main weatherData={weatherData} />
       </div>
-      <ModalWithForm />
+      <ModalWithForm title="New Garment" buttonText="Add Garment">
+        <label htmlFor="name" className="modal__label">
+          Name{" "}
+          <input
+            type="text"
+            className="modal__input"
+            id="name"
+            placeholder="name"
+          />
+        </label>
+        <label htmlFor="imageURL" className="modal__label">
+          Image{" "}
+          <input
+            type="URL"
+            className="modal__input"
+            id="imageURL"
+            placeholder="Image URL"
+          />
+        </label>
+        <fieldset className="modal__radio-buttons">
+          <legend className="modal__legend">Select the weather type;</legend>
+          <label htmlFor="hot" className="modal__label modal__label_type_radio">
+            <input type="radio" className="modal__radio-input" id="hot" /> Hot
+          </label>
+          <label
+            htmlFor="warm"
+            className="modal__label modal__label_type_radio"
+          >
+            <input type="radio" className="modal__radio-input" id="warm" /> Warm
+          </label>
+          <label
+            htmlFor="cold"
+            className="modal__label modal__label_type_radio"
+          >
+            <input type="radio" className="modal__radio-input" id="cold" /> Cold
+          </label>
+        </fieldset>
+      </ModalWithForm>
     </div>
   );
 }
