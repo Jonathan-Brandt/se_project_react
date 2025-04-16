d83a0094d7becfa0de8de9298cbbae1682ef6274
Jonathan Brandt <jwilliambrandtwork@gmail.com>
2025-04-16 15:30:41 -0500

    Project-10 Final Stages

--- src/utils/weatherAPI.js
+++ src/utils/weatherAPI.js
@@ -15,7 +15,7 @@ export const filterWeatherData = (data) => {
   result.city = data.name;
   result.temp = { F: data.main.temp };
   result.type = getWeatherType(result.temp.F);
-  result.condition = data.weather[0].main.to;
+  result.condition = data.weather[0].main;
   result.isDay = isDay(data.sys, Date.now());
   return result;
 };
