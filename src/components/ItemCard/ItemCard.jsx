8b6de51aa9ad4a38bdb176c4104609fb766b4ccc
Jonathan Brandt <jwilliambrandtwork@gmail.com>
2025-10-22 17:47:14 -0500

    Project 14 like button styling wip

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -1,6 +1,8 @@
 import "./ItemCard.css";
+
 function ItemCard({ item, cardClick, handleCardLike }) {
   const isLiked = item.likes.some((id) => id === currentUser?._id);
+  const itemLikeButtonClassName = `...`;
 
   const handleLike = (e) => {
     e.stopPropagation();
@@ -11,9 +13,15 @@ function ItemCard({ item, cardClick, handleCardLike }) {
       <div className="item-card__name-container">
         <h2 className="item-card__name">{item.name}</h2>
       </div>
+      <div className="like-button__container">
+        <button
+          handleLike={() => handleLike(item)}
+          className="like-button"
+          type="button"
+        ></button>
+      </div>
       <img
         onClick={() => cardClick(item)}
-        handleLike={() => handleLike(item)}
         className="item-card__image"
         src={item.imageUrl}
         alt={item.name}
