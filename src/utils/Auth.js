da7ccebb1c6b3e69b28fbdb3954920fcc606e9d3
Jonathan Brandt <jwilliambrandtwork@gmail.com>
2025-11-20 17:03:00 -0600

    Project 15 front end deployment

--- src/utils/Auth.js
+++ src/utils/Auth.js
@@ -1,5 +1,8 @@
 import { getResponse } from "./API";
-const BASE_URL = "http://localhost:3001";
+const BASE_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://api.wtwrjb.jumpingcrab.com"
+    : "http://localhost:3001";
 
 export function signup({ name, avatar, email, password }) {
   return fetch(`${BASE_URL}/signup`, {
