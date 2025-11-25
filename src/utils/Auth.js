c37216e6ac6297b8db6c22c293114201993c716e
Jonathan Brandt <jwilliambrandtwork@gmail.com>
2025-11-25 16:04:55 -0600

    Projet 25 final touches WIP

--- src/utils/Auth.js
+++ src/utils/Auth.js
@@ -1,11 +1,11 @@
 import { getResponse } from "./API";
-const BASE_URL =
+const baseUrl =
   process.env.NODE_ENV === "production"
     ? "https://api.wtwrjb.jumpingcrab.com"
     : "http://localhost:3001";
 
 export function signup({ name, avatar, email, password }) {
-  return fetch(`${BASE_URL}/signup`, {
+  return fetch(`${baseUrl}/signup`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ name, avatar, email, password }),
@@ -13,7 +13,7 @@ export function signup({ name, avatar, email, password }) {
 }
 
 export function signin({ email, password }) {
-  return fetch(`${BASE_URL}/signin`, {
+  return fetch(`${baseUrl}/signin`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ email, password }),
